b8b42565c41f3df15b11fe7579bad29c4467c9cd
Yevhen Pinchuk <yevhenii.pinchuk@digitalturbine.com>
2023-09-20 18:59:57 +0300

    init

---
+++ src/main/java/com/company/juniorcrudservice/JuniorCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.juniorcrudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorCrudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuniorCrudServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/juniorcrudservice/JuniorCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.juniorcrudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorCrudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
