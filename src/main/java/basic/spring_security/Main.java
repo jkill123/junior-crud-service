87049f77bed4d7b02521da49c8b9fdba2f5f0575
Yevhen Pinchuk <yevhenii.pinchuk@digitalturbine.com>
2024-01-12 14:08:28 +0200

    XXXXX | added security

---
+++ src/main/java/basic/spring_security/Main.java
@@ -0,0 +1,12 @@
+package basic.spring_security;
+
+public class Main {
+    //todo use started to edit config instead of create from scratch
+    // 2 default user + generated password
+    // 3 wow we can see session after login
+    // 4 curl -> dispatcher servlet -> call map(path, controller) -> redirect to controller
+    // 5 filter chaining google cases
+    // 6 password encoder
+    // 7 last
+    // 8 roles
+}
---
+++ src/main/java/com/company/juniorcrudservice/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.company.juniorcrudservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.formLogin(form ->
+                // loginPage("/login")
+                        form.permitAll())
+                .authorizeRequests(authorize -> authorize
+                        .regexMatchers("/").permitAll()
+                        .regexMatchers( "/api/v1/table/save").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .logout(logout -> logout.permitAll())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //user input password in form
+        System.out.println(encoder.encode("password"));
+        boolean isMatchPassword = encoder.matches("password", "{bcrypt}$2a$10$wsn2Z3ylDAKKeNaSoeMfkeneWlCr36qNw1RyunT3vGAY4ZSUEJub.");
+        System.out.println(isMatchPassword);
+
+        UserDetails user =
+                User
+                        .withUsername("user")
+                        .password("{bcrypt}$2a$10$wsn2Z3ylDAKKeNaSoeMfkeneWlCr36qNw1RyunT3vGAY4ZSUEJub.")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
