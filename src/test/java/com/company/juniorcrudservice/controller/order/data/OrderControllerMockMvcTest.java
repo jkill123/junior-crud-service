b4f380d0ad95cad0b3e7a6c67167944f769c6773
Yevhen Pinchuk <yevhenii.pinchuk@digitalturbine.com>
2024-03-13 12:02:29 +0200

    XXXXX | added integration tests

--- src/test/java/com/company/juniorcrudservice/controller/order/data/OrderControllerMockMvcTest.java
+++ src/test/java/com/company/juniorcrudservice/controller/order/data/OrderControllerMockMvcTest.java
@@ -1,42 +1,43 @@
-//package com.company.juniorcrudservice.controller.order.data;
-//
-//import com.company.juniorcrudservice.dto.OrderDto;
-//import com.company.juniorcrudservice.service.order.OrderService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.List;
-//
-//import static java.util.Collections.emptyList;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@ActiveProfiles("dev")
-//class OrderControllerMockMvcTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private OrderService orderService;
-//
-//    @Test
-//    void shouldGetOrders() throws Exception {
-//        when(orderService.getOrders()).thenReturn(List.of(OrderDto.builder()
-//                .id(11)
-//                .cost(11.1)
-//                .products(emptyList())
-//                .build()));
-//
-//        this.mockMvc.perform(get("/api/v1/orders")).andDo(print()).andExpect(status().isOk());
-//    }
-//}
+package com.company.juniorcrudservice.controller.order.data;
+
+import com.company.juniorcrudservice.dto.OrderDto;
+import com.company.juniorcrudservice.service.order.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class OrderControllerMockMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void shouldGetOrders() throws Exception {
+        when(orderService.getOrders(any())).thenReturn(List.of(OrderDto.builder()
+                .id(11)
+                .cost(11.1)
+                .products(emptyList())
+                .build()));
+
+        this.mockMvc.perform(get("/api/v1/orders")).andDo(print()).andExpect(status().isOk());
+    }
+}
